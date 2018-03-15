f57d95a451098f84bc456e01bb31a1eec3ec4f86
gg123gg123 <greg-g17@hotmail.co.uk>
2018-03-15 11:47:07 +0000

    Final Branch

--- register.php
+++ register.php
@@ -4,39 +4,143 @@
 <html>
 
 <head>
-    <title>DustBusters</title>
-    <link rel="stylesheet" href="styles.css">
+
+
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+    <!--
+    Kool Store Template
+    http://www.templatemo.com/preview/templatemo_428_kool_store
+    -->
+        <meta charset="utf-8">
+        <title> DustBusters - eCommerce Store</title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo-misc.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+
 </head>
-
+</head>
 <body>
-    <header class="navigation">
-        <div class="logo"><img src="img/logo.png" />
-        </div>
-        <div class="menu">
-            <a class="item" href="index.php">Home</a>
-            <a class="item" href="products.php">Products</a>
-            <a class="item" href="register.php">Register</a>
-            <a class="item" href="basket.php">Basket</a>
-        </div>
-    </header>
+    <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
 
-    
+
+    <header class="site-header">
+        <div class="top-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="top-header-left">
+                            <a href="#">Sign Up</a>
+                            <a href="#">Log In</a>
+                        </div> <!-- /.top-header-left -->
+                    </div> <!-- /.col-md-6 -->
+                    <div class="col-md-6 col-sm-6">
+                        <div class="social-icons">
+                            <ul>
+                                <li><a href="#" class="fa fa-facebook"></a></li>
+                                <li><a href="#" class="fa fa-dribbble"></a></li>
+                                <li><a href="#" class="fa fa-twitter"></a></li>
+                                <li><a href="#" class="fa fa-linkedin"></a></li>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div> <!-- /.social-icons -->
+                    </div> <!-- /.col-md-6 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.top-header -->
+        <div class="main-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-8">
+                        <div class="logo">
+                            <h1><a href="#">DustBusters</a></h1>
+                        </div> <!-- /.logo -->
+                    </div> <!-- /.col-md-4 -->
+                    <div class="col-md-8 col-sm-6 col-xs-4">
+                        <div class="main-menu">
+                            <a href="#" class="toggle-menu">
+                                <i class="fa fa-bars"></i>
+                            </a>
+                            <ul class="menu">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="products.php">Products</a></li>
+                                <li><a href="register.php">Register</a></li>
+                                <li><a href="basket.php">Basket</a></li>
+                                <li><a href="#">About</a></li>
+                            </ul>
+                        </div> <!-- /.main-menu -->
+                    </div> <!-- /.col-md-8 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.main-header -->
+        <div class="main-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-7">
+                        <div class="list-menu">
+                            <ul>
+                                <li><a href="products.php">Shop</a></li>
+                                <li><a href="product-detail.html">Details </a></li> <!--temp tbc -->
+                                <li><a href="contact.html">Contact </a></li> <!--temp tbc -->
+                            </ul>
+                        </div> <!-- /.list-menu -->
+                    </div> <!-- /.col-md-6 -->
+                    <div class="col-md-6 col-sm-5">
+                        <div class="notification">
+                            <span>Save 10% on any product by using code:10OFF</span>
+                        </div>
+                    </div> <!-- /.col-md-6 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.main-nav -->
+    </header> <!-- /.site-header -->
+<body>
+
+
+
     <font color="#ddd">
     <div class="main">
         <div class="register">
-            <p>Sign up to dustbusters.</p>
+            <h2><p>Sign up to dustbusters</p><h2>
+            </div>
         <form action="reg.php" method="post">
-            <p><label>Email address:</label><input type="text" name="email"/></p>
+          <font color="#000">
+          <div class="register">
+            <p><label>Email:</label><input type="email" required="" aria-required="true" aria-invalid="true" name="email"/></p>
             <p><label>First Name:</label><input type="text" name="fname"/></p>
             <p><label>Surname:</label> <input type="text" name="sname"/> </p>
             <p><label>Post Code:</label> <input type="text" name="pcode"/></p>
-            <p><label>Password:</label> <input type="password" name="pass" maxlength="25" /> 25 character limit</p>
-            <p><input type="submit" value="Register" /></p>
+            <p><<label>Password:</label>
+<input type="password" name="password" id="password">
+<span id="passwordmeter"><span></span></span>
+<span id="passwordmessage" aria-live="polite"></span>
+              <div aria-live="assertive" class="form-message form-message--error form-message--password field__error"
+              id="form-message-password"><p class="form-message__text"><span><span>Something's missing</span><span>. </span><span>Please check and try again.</span></span>
+              </p></div>
+              <div>
+ <p><input id="btn" type="submit"  value="Register" /></p>
         </form>
         </div>
-        
+
            <div class="login">
-            <p> login to your DustBusters account.</p>
+            <h2><p>
+               Login to your DustBusters account </p><h2>
+               </div>
+               <div class="login"
             <form action="login.php" method="post">
             <p><label>Email address:</label><input type="text" name="email"/></p>
                 <p><label>Password:</label><input type="password" name="pass"/></p>
@@ -44,14 +148,9 @@
             </form>
         </div>
     </div>
-    
-    <center>
-    <div class="footer">Created By:Alex 
-        </div></center>
-   
 
+    <center>
+    <div class="footer">Add links
 
 </body>
-
 </html>
-
