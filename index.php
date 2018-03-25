97c66e713a0d680701f900921c2bf385bac32c71
gg123gg123 <greg-g17@hotmail.co.uk>
2018-03-25 20:44:09 +0100

    message

--- index.php
+++ index.php
@@ -116,21 +116,22 @@
                                 </div> <!-- /.container -->
                             </div> <!-- /.main-nav -->
                         </header>
-                        <body>
+
     <div align="center">
         <div class="description">
             <p align="left">Hi, welcome to DustBusters A FAKE E-Commerce business commited to selling high quality vacuum cleaners. We sell a wide range of vacuum cleaners to suit all of your needs, each of our vacuums come with a 1 year warrenty which includes parts and labour on top of our 30 day money back guarentee plus we will collect your old vacuum free of charge. We care about the planet and as such we ensure all excess packaging is recycled along with your old vacuum cleaner. </p>
 
             <p align="left">We sell vacuums on behalf of the industrys leading innovators including Dyson,Vax and &quot;Henry Hoover&quot;. Dustbusters is a family run business who have been operating for nearly 10 years, We pride ourselves on our reputatation and belive that customer satisfaction is key so if you have any questions regarding the products for sale on our website dont hesitate to contact our award winning customer support.</p>
 
-            <p align="left">We have designed this new website to accomodate all users with simplicity and elegance, the navigation bar at the top can be used to access all of the web pages filled with usefull information.If you wish to purchase a vaccum simply sighn up using the registration page at the top thank you for taking the time to read our introduction and we hope you find the perfect vacuum and if you already have we thank you for your business and hope to see you in the future.
+            <p align="left">We have designed this new website to accomodate all users with simplicity and elegance, the navigation bar at the top can be used to access all of the web pages filled with useful information.If you wish to purchase a vaccum simply sign up using the registration page at the top <br>
+
+              Thank you for taking the time to read our introduction and we hope you find the perfect vacuum and if you already have we thank you for your business and hope to see you in the future.
             </p>
         <div id="homevideo"><iframe width="560" height="315" src="https://www.youtube.com/embed/oguKCHP7jNQ" frameborder="0" allowfullscreen></iframe></div>
         </div>
         <div class="banner"><img src="img/Vacuum%20banner.jpg" />
         </div>
-        <center>
-          <section></section>
+
 
           <footer class="footer">
               <div class="div-block-3"><a class="footer-link" href="#">No fear act</a><a class="footer-link" href="#">Terms and conditions</a><a class="footer-link" href="#">Privacy</a>
@@ -171,5 +172,5 @@
                   <script src="js/bootstrap.js"></script>
                   <script src="js/plugins.js"></script>
                   <script src="js/main.js"></script>
-          </body>
+
           </html>
